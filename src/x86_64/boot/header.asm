24b3fdbaba87b7c5d758561315baf396a7b6a66b
Dawid Stasiak <dawid.stasiak.dc@proton.me>
2024-02-03 00:54:37 +0100

    Makefile fixes - ditch dockerfile

--- src/x86_64/boot/header.asm
+++ src/x86_64/boot/header.asm
@@ -1,4 +1,4 @@
-section .multiboot2_header
+section .multiboot_header
 header_start:
     ; magic number 
     dd 0xe85250d6 ; multiboot2
@@ -8,7 +8,8 @@ header_start:
     dd header_end - header_start ; calculation
     ; checksum
     dd 0x100000000 - (0xe85250d6 + 0 + (header_end - header_start))
-    ;end 
+
+    ; end 
     dw 0
     dw 0
     dd 8
--- src/x86_64/boot/main.asm
+++ src/x86_64/boot/main.asm
@@ -3,5 +3,5 @@ global start
 section .text
 bits 32
 start:
-    mov dword [0xb8000], 0x2f4v2f4f ; OK
+    mov dword [0xb8000], 0x2f4b2f4f ; OK
     hlt
